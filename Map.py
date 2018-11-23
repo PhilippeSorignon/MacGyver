b4cfffcb827e3aa2676568c9dfa4d5189472972c
Philippe <philippe.sorignon@protonmail.com>
2018-11-23 15:27:08 +0100

    Initialization and display of the Map class

--- Map.py
+++ Map.py
@@ -1,11 +1,25 @@
 class Map:
     def __init__(self):
-        pass
+        self.map = []
+        map_file = open("map.txt", "r")
+        lines = map_file.readlines()
+
+        for read_line in lines:
+            tab = []
+            for i in range(0, 15):
+                tab.append(read_line[i])
+            self.map.append(tab)
+
+        self.display()
 
     def display(self):
-        pass
+        for line in range(0, 15):
+            line_tab = ""
+            for column in range(0, 15):
+                line_tab += self.map[line][column]
+            print(line_tab)
 
-    def is_empty(self):
+    def is_empty(self, line, column):
         pass
 
     def move_sprite(self, before, after):
